4449936270c61b0977a94f9a7cf4e33b36278cb8
Anthony D'Andrea <anthony.dandrea@me.com>
2016-05-19 21:42:34 -0700

    make youtube green btn disappear when played

--- src/scripts/youtube.js
+++ src/scripts/youtube.js
@@ -1,8 +1,15 @@
 // da button
 var ytButton = document.getElementById('play-youtube');
+// remove da btn func
+function removeYTButton() {
+  var ytButtonMaybe = document.getElementById('play-youtube');
+  if (ytButtonMaybe) {
+    ytButton.parentNode.removeChild(ytButton);
+  }
+}
 // safari fucks it all up so we just remove the button
 if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-  ytButton.parentNode.removeChild(ytButton);
+  removeYTButton();
 }
 // load youtubery async
 var tag = document.createElement('script');
@@ -23,10 +30,14 @@ function onPlayerReady() {
     }
   });
 }
+function onPlayerStateChange() {
+  removeYTButton();
+}
 window.onYouTubePlayerAPIReady = function() {
   player = new YT.Player('yt-video', {
     events: {
-      'onReady': onPlayerReady
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
     }
   });
 }
