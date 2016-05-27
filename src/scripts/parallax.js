2a01fef032faf054eafdf8556edacdaeb0925fdb
Anthony D'Andrea <anthony.dandrea@me.com>
2016-05-26 20:55:50 -0700

    feedback

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -5,18 +5,19 @@
 })();
 
 // handle bg sizing
-(function() {
-  var bg = document.getElementsByClassName('parallax-bg')[0];
-  var body = document.body;
-  var html = document.documentElement;
-  function setHeight() {
-    var windowHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-    bg.style.height = windowHeight+'px';
-  }
-  // This is fucking dumb but it doesn't correctly resize all of the time first try
-  window.addEventListener('resize', setHeight);
-  document.addEventListener('DOMContentLoaded', setHeight);
-  setTimeout(setHeight, 500);
-  setInterval(setHeight, 1000);
-  setHeight();
-})();
+// (function() {
+//   var bg = document.getElementsByClassName('parallax-bg')[0];
+//   var body = document.body;
+//   var html = document.documentElement;
+//   function setHeight() {
+//     var windowHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+//     console.log('windowHeight', windowHeight);
+//     bg.style.height = windowHeight+'px';
+//   }
+//   // This is fucking dumb but it doesn't correctly resize all of the time first try
+//   window.addEventListener('resize', setHeight);
+//   document.addEventListener('DOMContentLoaded', setHeight);
+//   setTimeout(setHeight, 500);
+//   // setInterval(setHeight, 1000);
+//   setHeight();
+// })();
